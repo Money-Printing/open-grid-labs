f35879c96e14f8b945f3f758dc94afec6ef898d3
hisuperaman <theaman1805@gmail.com>
2025-11-26 11:47:17 +0530

    remove unused import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, HashRouter } from 'react-router'
+import { HashRouter } from 'react-router'
 import ScrollToTop from './components/scroll-to-top/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
